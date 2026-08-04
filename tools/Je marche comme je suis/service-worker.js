60bd8b31165c911bcebd9cf76048981f5c5e6bba
11drumboy11 <11drumboy11@gmail.com>
2026-08-04 18:23:21 +0200

    Create service-worker.js

---
+++ tools/Je marche comme je suis/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = "jmmjs-shell-d045-v1";
+const SHELL = ["./", "./je-marche-comme-je-suis-p0.html", "./manifest.webmanifest"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+    if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    }
+    return response;
+  }).catch(() => caches.match("./je-marche-comme-je-suis-p0.html"))));
+});
