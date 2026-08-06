231a8e8a80ee503fe3c2dde32f3b0fe1d70d4352
11drumboy11 <11drumboy11@gmail.com>
2026-08-06 12:21:58 +0200

    Update service-worker.js

--- tools/Je marche comme je suis/service-worker.js
+++ tools/Je marche comme je suis/service-worker.js
@@ -1,18 +1,56 @@
-const CACHE = "jmmjs-shell-charte-v1-20260806-fix-icons-v2";
-const SHELL = ["./je-marche-comme-je-suis-p0.html", "./manifest.webmanifest", "./icons/jmmjs-icon.svg", "./icons/favicon-32.png", "./icons/apple-touch-icon-180.png", "./icons/jmmjs-icon-192.png", "./icons/jmmjs-icon-512.png"];
+const CACHE = "jmmjs-shell-20260806-routing-fix";
+const SHELL = [
+  "./je-marche-comme-je-suis-p0.html",
+  "./manifest.webmanifest",
+  "./icons/jmmjs-icon.svg",
+  "./icons/favicon-32.png",
+  "./icons/apple-touch-icon-180.png",
+  "./icons/jmmjs-icon-192.png",
+  "./icons/jmmjs-icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()),
+  );
 });
+
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
 });
+
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    if (response && response.ok && new URL(event.request.url).origin === self.location.origin) {
-      const copy = response.clone();
-      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-    }
-    return response;
-  }).catch(() => caches.match("./je-marche-comme-je-suis-p0.html"))));
+  const request = event.request;
+  if (request.method !== "GET") return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          if (response?.ok) {
+            const copy = response.clone();
+            event.waitUntil(caches.open(CACHE).then((cache) => cache.put(request, copy)));
+          }
+          return response;
+        })
+        .catch(() => caches.match(request).then((cached) => cached || caches.match("./je-marche-comme-je-suis-p0.html"))),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response?.ok) {
+        const copy = response.clone();
+        event.waitUntil(caches.open(CACHE).then((cache) => cache.put(request, copy)));
+      }
+      return response;
+    })),
+  );
 });
