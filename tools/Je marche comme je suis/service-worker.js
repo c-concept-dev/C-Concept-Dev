439132a2ea7eb597e987a5309a63c76a4e4bc669
11drumboy11 <11drumboy11@gmail.com>
2026-08-16 22:06:26 +0200

    Update service-worker.js

--- tools/Je marche comme je suis/service-worker.js
+++ tools/Je marche comme je suis/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "jmmjs-shell-20260816-d101f-mobile-first";
+const CACHE = "jmmjs-shell-20260816-d101h-desktop-services-fix";
 const SHELL = [
   "./je-marche-comme-je-suis-p0.html",
   "./manifest.webmanifest",
